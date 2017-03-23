ce20d35e6a6df3a0509dedc1846db4bb04134e7e
liquangang <liquangang@idstaff.com>
2017-03-23 19:08:47 +0800

    1.提交工程基础文件

---
+++ GpuimageTeset/GpuimageTeset/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GpuimageTeset
+//
+//  Created by 必要 on 2017/3/23.
+//  Copyright © 2017年 必要. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GpuimageTeset/GpuimageTeset/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GpuimageTeset
+//
+//  Created by 必要 on 2017/3/23.
+//  Copyright © 2017年 必要. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
